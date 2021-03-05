f83be878b59523429971824eb7f8fc73f85cc7ff
suihongwei <suihongwei@konka.com>
2021-03-05 11:21:51 +0800

    feat:LeetCode232

---
+++ src/FuckingBinaryTree/LeetCode232.java
@@ -0,0 +1,82 @@
+package FuckingBinaryTree;
+
+
+import java.util.Stack;
+
+/**
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列的支持的所有操作（push、pop、peek、empty）：
+ */
+public class LeetCode232 {
+    public static void main(String[] args) {
+        MyQueue obj = new MyQueue();
+        obj.push(1);
+        obj.push(2);
+        int param_3 = obj.peek();
+        int param_2 = obj.pop();
+        boolean param_4 = obj.empty();
+
+        System.out.println(param_2 + "" + param_3 + "" + param_4);
+    }
+}
+
+class MyQueue {
+
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+    private int front = 0;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        if (stack1.empty()) {
+            front = x;
+        }
+        stack1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (!stack2.empty()) {
+            return stack2.peek();
+        }
+        return front;
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack2.empty() && stack1.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
