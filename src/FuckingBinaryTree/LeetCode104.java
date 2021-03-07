7855afec38a93000c58aef02f330082be90358bc
suihongwei <suihongwei@konka.com>
2021-03-07 23:23:08 +0800

    feat:LeetCode104

---
+++ src/FuckingBinaryTree/LeetCode104.java
@@ -0,0 +1,26 @@
+package FuckingBinaryTree;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+public class LeetCode104 {
+
+    public static void main(String[] args) {
+
+    }
+    
+    //第一时间竟然没有写出来，我是傻逼
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left,right) + 1;
+        }
+    }
+}
