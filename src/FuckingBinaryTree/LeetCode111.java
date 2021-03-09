30980a32a7a7204309e2dbbab87bb62e12e02e21
suihongwei <suihongwei@konka.com>
2021-03-09 20:24:20 +0800

    feat:LeetCode111

---
+++ src/FuckingBinaryTree/LeetCode111.java
@@ -0,0 +1,27 @@
+package FuckingBinaryTree;
+
+/**
+ * 给定一个二叉树，找出其最小深度。
+ * <p>
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * <p>
+ * 说明：叶子节点是指没有子节点的节点。
+ */
+public class LeetCode111 {
+    public static void main(String[] args) {
+
+    }
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
