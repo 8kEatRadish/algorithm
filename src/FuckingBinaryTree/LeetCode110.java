659ee42e1dcb008ed3d636f25395d85bc7ac9f96
suihongwei <suihongwei@konka.com>
2021-03-09 19:59:37 +0800

    feat:LeetCode110

---
+++ src/FuckingBinaryTree/LeetCode110.java
@@ -0,0 +1,31 @@
+package FuckingBinaryTree;
+
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * <p>
+ * 本题中，一棵高度平衡二叉树定义为：
+ * <p>
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ */
+public class LeetCode110 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+}
