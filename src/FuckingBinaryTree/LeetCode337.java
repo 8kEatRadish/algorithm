fbfc0a0a23341138845f1fa13b1628d667fd0ff0
suihongwei <18845795735@163.com>
2021-03-17 18:59:59 +0800

    feat:LeetCode337

---
+++ src/FuckingBinaryTree/LeetCode337.java
@@ -0,0 +1,43 @@
+package FuckingBinaryTree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+ * <p>
+ * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/house-robber-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode337 {
+
+
+    //这里f（o）表示选择o节点的最大权重和
+    //这里g（o）表示不选择o节点最大权重和
+
+    //f(o) = g(o.left) + g(o.right) 因为选择了o节点，他的两个子节点就不可以选择
+    //g(0) = Math.max(f(o.left),g(o.left)) + Math.max(f(o.right),g(o.right)) 不选择o节点，他的子节点可选择，也可不选择
+    Map<TreeNode, Integer> f = new HashMap<>();
+    Map<TreeNode, Integer> g = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        dfs(root.left);
+        dfs(root.right);
+
+        //选择了root，所以求和的时候要把root.val算进去
+        f.put(root, root.val + g.getOrDefault(root.left, 0) + g.getOrDefault(root.right, 0));
+        g.put(root, Math.max(f.getOrDefault(root.left, 0), g.getOrDefault(root.left, 0)) + Math.max(f.getOrDefault(root.right, 0), g.getOrDefault(root.right, 0)));
+    }
+}
