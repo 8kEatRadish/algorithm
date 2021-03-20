98bd1907fc6af99d6ed95fb2131d625cd5729a54
suihongwei <18845795735@163.com>
2021-03-20 13:01:14 +0800

    feat:LeetCode95

---
+++ src/FuckingBinaryTree/LeetCode95.java
@@ -0,0 +1,42 @@
+package FuckingBinaryTree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ */
+public class LeetCode95 {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTree = new LinkedList<>();
+        if (start > end) {
+            //忘记了添加空节点
+            allTree.add(null);
+            return allTree;
+        }
+        for (int i = start; i <= end; i++) {
+
+            List<TreeNode> leftTree = generateTrees(start, i - 1);
+            List<TreeNode> rightTree = generateTrees(i + 1, end);
+
+            for (TreeNode left : leftTree) {
+                for (TreeNode right : rightTree) {
+                    TreeNode curr = new TreeNode(i);
+                    curr.left = left;
+                    curr.right = right;
+                    allTree.add(curr);
+                }
+            }
+
+        }
+
+        return allTree;
+    }
+}
