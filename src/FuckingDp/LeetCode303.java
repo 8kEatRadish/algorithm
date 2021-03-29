a9b9fbabccef3dfdf656b6af1fca3de4709fe4c9
suihongwei <18845795735@163.com>
2021-03-29 17:38:57 +0800

    feat:LeetCode32

---
+++ src/FuckingDp/LeetCode303.java
@@ -0,0 +1,30 @@
+package FuckingDp;
+
+/**
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+ *
+ * 实现 NumArray 类：
+ *
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode303 {
+
+    //用前缀和 sums[n]表示前n个数字的和
+    int[] sums;
+    public void NumArray(int[] nums) {
+        sums = new int[nums.length + 1];
+        sums[0] = 0;
+        for (int i = 0; i < nums.length; i++){
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sums[right + 1] - sums[left];
+    }
+}
