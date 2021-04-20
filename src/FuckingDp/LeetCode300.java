c42e4a3d5f79e2f8d08c3a023c1259535a03666d
suihongwei <18845795735@163.com>
2021-04-20 16:07:57 +0800

    feat:LeetCode300

--- src/FuckingDp/LeetCode300.java
+++ src/FuckingDp/LeetCode300.java
@@ -12,7 +12,9 @@ package FuckingDp;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class LeetCode300 {
+    public static void main(String[] args) {
 
+    }
     // public int lengthOfLIS(int[] nums) {
     //     if(nums.length == 0){
     //         return 0;
